93e245a0489c3e9a3aca3b89efa87611b4957ec8
Patrik Bartak <p.bartak@student.tudelft.nl>
2023-05-30 22:53:58 +0200

    Add additional use case

--- istio-config.sh
+++ istio-config.sh
@@ -1,9 +1,6 @@
 # istioctl install
 kubectl label ns default istio-injection=enabled
 kubectl get ns default --show-labels
-kubectl delete -f k8s/istio-deployment.yml
-kubectl apply -f k8s/istio-deployment.yml
-kubectl get pods
 # Addons
 kubectl apply -f ~/istio-1.17.2/samples/addons/prometheus.yaml
 kubectl apply -f ~/istio-1.17.2/samples/addons/jaeger.yaml
---
+++ istio-deploy.sh
@@ -0,0 +1,3 @@
+kubectl delete -f k8s/istio-deployment.yml
+kubectl apply -f k8s/istio-deployment.yml
+kubectl get pods
